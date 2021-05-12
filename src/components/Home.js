0336d61624608dea9f55a5a6ff0b305259defa01
siddharth0600 <siddharthtripathi00@gmail.com>
2021-05-12 13:45:50 +0530

    Viewers complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import ImgSlider from "./ImgSlider";
+import Viewers from "./Viewers";
 
 function Home() {
   return (
     <Container>
       <ImgSlider />
+      <Viewers />
     </Container>
   );
 }
@@ -15,7 +17,7 @@ export default Home;
 const Container = styled.main`
   min-height: calc(100vh - 70px);
   padding: 0 calc(3.5vw + 5px);
-  /* position: relative; */
+  position: relative;
   overflow-x: hidden;
 
   &:before {
