8814e0d6330713ee878f5d2193cd1d7adeb61109
siddharth0600 <siddharthtripathi00@gmail.com>
2021-05-13 23:12:18 +0530

    login feature added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,43 +1,175 @@
 /* eslint-disable react/style-prop-object */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/alt-text */
-import React from "react";
+// import React from "react";
+// import { auth, provider } from "../firebase";
+// import styled from "styled-components";
+// import {
+//   selectUserName,
+//   selectUserPhoto,
+//   setUserLogin,
+// } from "../features/user/userSlice";
+// import { useSelector, useDispatch } from "react-redux";
+
+// function Header() {
+//   const dispatch = useDispatch();
+//   const userName = useSelector(selectUserName);
+//   const userPhoto = useSelector(selectUserPhoto);
+
+//   const signIn = () => {
+//     auth.signInWithPopup(provider).then((result) => {
+//       console.log(result);
+//       let user = result.user;
+//       dispatch(
+//         setUserLogin({
+//           name: user.diplayName,
+//           photo: user.photoURL,
+//           email: user.email,
+//         })
+//       );
+//       console.log(user.displayName);
+//       console.log(user.photoURL);
+//       console.log(user.email);
+//     });
+//   };
+//   console.log(userName);
+//   return (
+//     <Nav>
+//       <Logo src="/images/logo.svg" />
+//       {!userName ? (
+//         <LoginContainer>
+//           <Login onClick={signIn}>LOGIN</Login>
+//         </LoginContainer>
+//       ) : (
+//         <>
+//           <NavMenu>
+//             <a>
+//               <img src="/images/home-icon.svg" />
+//               <span>HOME</span>
+//             </a>
+
+//             <a>
+//               <img src="/images/search-icon.svg" />
+//               <span>SEARCH</span>
+//             </a>
+
+//             <a>
+//               <img src="/images/watchlist-icon.svg" />
+//               <span>WATCHLIST</span>
+//             </a>
+
+//             <a>
+//               <img src="/images/original-icon.svg" />
+//               <span>ORIGINALS</span>
+//             </a>
+//             <a>
+//               <img src="/images/movie-icon.svg" />
+//               <span>MOVIES</span>
+//             </a>
+//             <a>
+//               <img src="/images/series-icon.svg" />
+//               <span>SERIES</span>
+//             </a>
+//           </NavMenu>
+//           <UserImg src="https://instagram.fdel8-1.fna.fbcdn.net/v/t51.2885-19/s150x150/151284041_1416444448725872_4793746364181559970_n.jpg?tp=1&_nc_ht=instagram.fdel8-1.fna.fbcdn.net&_nc_ohc=Ti65inApSiAAX-igRlL&edm=ABfd0MgBAAAA&ccb=7-4&oh=c57056e055d181e837fa55e387262fac&oe=60C08C25&_nc_sid=7bff83" />
+//         </>
+//       )}
+//     </Nav>
+//   );
+// }
+
+import React, { useEffect } from "react";
 import styled from "styled-components";
+import {
+  selectUserName,
+  setUserLogin,
+  selectUserPhoto,
+  setSignOut,
+} from "../features/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { auth, provider } from "../firebase";
+import { useHistory } from "react-router-dom";
 
 function Header() {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const userName = useSelector(selectUserName);
+  const userPhoto = useSelector(selectUserPhoto);
+
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        dispatch(
+          setUserLogin({
+            name: user.displayName,
+            email: user.email,
+            photo: user.photoURL,
+          })
+        );
+        history.push("/");
+      }
+    });
+  }, []);
+
+  const signIn = () => {
+    auth.signInWithPopup(provider).then((result) => {
+      let user = result.user;
+      dispatch(
+        setUserLogin({
+          name: user.displayName,
+          email: user.email,
+          photo: user.photoURL,
+        })
+      );
+      history.push("/");
+    });
+  };
+
+  const signOut = () => {
+    auth.signOut().then(() => {
+      dispatch(setSignOut());
+      history.push("/login");
+    });
+  };
+
   return (
     <Nav>
       <Logo src="/images/logo.svg" />
-      <NavMenu>
-        <a>
-          <img src="/images/home-icon.svg" />
-          <span>HOME</span>
-        </a>
-
-        <a>
-          <img src="/images/search-icon.svg" />
-          <span>SEARCH</span>
-        </a>
-
-        <a>
-          <img src="/images/watchlist-icon.svg" />
-          <span>WATCHLIST</span>
-        </a>
-
-        <a>
-          <img src="/images/original-icon.svg" />
-          <span>ORIGINALS</span>
-        </a>
-        <a>
-          <img src="/images/movie-icon.svg" />
-          <span>MOVIES</span>
-        </a>
-        <a>
-          <img src="/images/series-icon.svg" />
-          <span>SERIES</span>
-        </a>
-      </NavMenu>
-      <UserImg src="https://instagram.fdel8-1.fna.fbcdn.net/v/t51.2885-19/s150x150/151284041_1416444448725872_4793746364181559970_n.jpg?tp=1&_nc_ht=instagram.fdel8-1.fna.fbcdn.net&_nc_ohc=Ti65inApSiAAX-igRlL&edm=ABfd0MgBAAAA&ccb=7-4&oh=c57056e055d181e837fa55e387262fac&oe=60C08C25&_nc_sid=7bff83" />
+      {!userName ? (
+        <LoginContainer>
+          <Login onClick={signIn}>Login</Login>
+        </LoginContainer>
+      ) : (
+        <>
+          <NavMenu class="topnav" id="myTopnav">
+            <a>
+              <img src="/images/home-icon.svg" alt="" />
+              <span>HOME</span>
+            </a>
+            <a>
+              <img src="/images/search-icon.svg" alt="" />
+              <span>SEARCH</span>
+            </a>
+            <a>
+              <img src="/images/watchlist-icon.svg" alt="" />
+              <span>WATCHLIST</span>
+            </a>
+            <a>
+              <img src="/images/original-icon.svg" alt="" />
+              <span>ORIGINALS</span>
+            </a>
+            <a>
+              <img src="/images/movie-icon.svg" alt="" />
+              <span>MOVIES</span>
+            </a>
+            <a>
+              <img src="/images/series-icon.svg" alt="" />
+              <span>SERIES</span>
+            </a>
+          </NavMenu>
+          <UserImg onClick={signOut} src={userPhoto} />
+        </>
+      )}
     </Nav>
   );
 }
@@ -118,4 +250,27 @@ const UserImg = styled.img`
   height: 58px;
   border-radius: 50%;
   cursor: pointer;
+`;
+
+const Login = styled.div`
+  border: 1px solid #f9f9f9;
+  padding: 8px 16px;
+  border-radius: 4px;
+  letter-spacing: 1.5px;
+  text-transform: uppercase;
+  background-color: rgba(0, 0, 0, 0.6);
+  transition: all 0.2s ease 0s;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
+  }
+`;
+
+const LoginContainer = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
 `;
